7458d7d41ac48b6a15c35bec5537306edbde0a5b
Stilic <lateman-jpeg@outlook.fr>
2021-10-14 07:08:09 +0200

    init

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+/** @type {import('@sveltejs/kit').Config} */
+import sveltePreprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
+const config = {
+	preprocess: sveltePreprocess({
+		postcss: {
+			plugins: [autoprefixer()]
+		}
+	})
+};
+
+export default config;

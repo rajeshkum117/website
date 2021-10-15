9c10bf26d7d9b2c186eaab9e407baf4902924590
Stilic <lateman-jpeg@outlook.fr>
2021-10-15 11:44:03 +0000

    fixin

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
 /** @type {import('@sveltejs/kit').Config} */
 import sveltePreprocess from 'svelte-preprocess';
 import autoprefixer from 'autoprefixer';
+import livereload from 'rollup-plugin-livereload';
+
 const config = {
 	preprocess: sveltePreprocess({
 		postcss: {
 			plugins: [autoprefixer()]
 		}
-	})
+	}),
+	plugins: [
+		!production &&
+			livereload({
+				watch: 'public',
+				clientUrl: process.env.CLIENT_URL
+			})
+	]
 };
 
 export default config;
